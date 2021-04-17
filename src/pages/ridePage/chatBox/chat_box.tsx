ad1775c4baf534318093fd170e6c0096c47f244d
Jvalaj <72438993+jvalaj@users.noreply.github.com>
2021-04-18 00:40:14 +0530

    cefcec

--- src/pages/ridePage/chatBox/chat_box.tsx
+++ src/pages/ridePage/chatBox/chat_box.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function ChatBox() {
-  return <div></div>;
+  return <div className="container min-vh-50">
+    <div className="row-fluid min-vh-75">
+
+    </div>
+    <div className="row-fluid flex-grow-1 d-flex">
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Send a message" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+          <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+      </div>
+    </div>
+    </div>
+    ;
 }
